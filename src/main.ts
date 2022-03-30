a64e2c003fbb6ab26915f221577bca3334b8a3d1
Rojan Gh <sojaner@gmail.com>
2022-03-30 23:32:54 +0200

    Progress on action

--- src/main.ts
+++ src/main.ts
@@ -33,8 +33,6 @@ async function run(): Promise<void> {
       throw new Error(`The commit '${commit}' doesn't exist on branch '${branch}'.`);
     }
 
-    core.setOutput('commit', commit);
-
     const octokit = new Octokit();
 
     const context = github.context;
@@ -43,27 +41,28 @@ async function run(): Promise<void> {
 
     let page = 1;
 
-    let count;
-
     do {
 
       const pagedReleases = (await octokit.rest.repos.listReleases({ owner: context.repo.owner, repo: context.repo.repo, page, per_page: 100 })).data;
-
-      count = pagedReleases.length;
 
       releases.push(...pagedReleases.map(release => ({ tag: release.tag_name, branch: release.target_commitish, creation: Date.parse(release.created_at) })));
 
       page++;
 
-    } while (count > 0);
+    } while (releases.some(release => release.branch === branch) && (branch === 'main' || releases.some(release => release.branch && 'main')));
+
+    const prerelease = branch !== 'main';
 
     const previousVersion = releases.filter(release => release.branch === branch).sort((a, b) => b.creation - a.creation).reverse().map(release => release.tag).pop();
 
-    const lastMainVersion = branch === 'main' ? previousVersion : releases.filter(release => release.branch === 'main').sort((a, b) => b.creation - a.creation).reverse()
+    const lastMainVersion = !prerelease ? previousVersion : releases.filter(release => release.branch === 'main').sort((a, b) => b.creation - a.creation).reverse()
     
       .map(release => release.tag).pop();
 
+    //TODO: Calculate the next version
     const version = `v0.9.${Math.trunc(Math.random() * 10000)}`;
+
+    core.setOutput('commit', commit);
 
     core.setOutput('version', version);
 
@@ -75,4 +74,4 @@ async function run(): Promise<void> {
   }
 }
 
-run()
+run();
