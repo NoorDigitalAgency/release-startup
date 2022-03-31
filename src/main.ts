4959868a6b7679d7ec2d1f4b69e37fe75aaf404d
Rojan Gh <sojaner@gmail.com>
2022-03-31 11:26:08 +0200

    Progress

--- src/main.ts
+++ src/main.ts
@@ -3,70 +3,137 @@ import * as exec from '@actions/exec';
 import * as github from '@actions/github';
 import { Octokit } from '@octokit/rest';
 
+function wait(milliseconds: number) {
+
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
+
 async function run(): Promise<void> {
 
   try {
 
     const stage = core.getInput('stage', { required: true });
 
-    const commit = core.getInput('commit');
+    const reference = core.getInput('reference');
+
+    const hotfix = core.getBooleanInput('hotfix');
 
     if (!['production', 'beta', 'alpha'].includes(stage)) {
 
       throw new Error(`Invalid stage name '${stage}'.`);
     }
 
-    const branch = stage === 'alpha' ? 'develop' : stage === 'beta' ? 'release' : 'main';
+    if (hotfix && stage !== 'production') {
 
-    const detached = commit !== '' && commit !== branch;
+      throw new Error(`A hotfix can only be released on 'production' while '${stage}' is specified.`);
+    }
 
-    await exec.exec('git', ['fetch', '--all']);
+    const target = stage === 'alpha' ? 'develop' : stage === 'beta' ? 'release' : 'main';
 
-    const correctBranch = !detached || (await exec.getExecOutput('git', ['branch', '-r', '--contains', commit]))
+    const source = stage === 'alpha' || stage === 'beta' ? 'develop' : 'beta';
 
-      .stdout.split('\n').filter(line => line.trim() !== '').map(line => line.trim().split('/').pop())
+    const detached = !hotfix && reference !== '' && reference !== source;
 
-      .includes(branch);
+    if (detached) {
 
-    if (!correctBranch) {
+      await exec.exec('git', ['fetch', '--all']);
 
-      throw new Error(`The commit '${commit}' doesn't exist on branch '${branch}'.`);
+      const exists = !detached || (await exec.getExecOutput('git', ['branch', '-r', '--contains', reference]))
+
+        .stdout.split('\n').filter(line => line.trim() !== '').map(line => line.trim().split('/').pop())
+
+        .includes(source);
+
+      if (!exists) {
+
+        throw new Error(`The commit '${reference}' doesn't exist on the base branch '${source}'.`);
+      }
     }
 
     const octokit = new Octokit();
 
     const context = github.context;
 
+    if ((await octokit.repos.listBranches({ owner: context.repo.owner, repo: context.repo.repo })).data.some(branch => branch.name === reference)) {
+
+      throw new Error(`The hotfix branch '${reference}' doesn't exist.'`);
+    }
+
     const releases = [];
 
     let page = 1;
 
+    let count;
+
     do {
 
-      const pagedReleases = (await octokit.rest.repos.listReleases({ owner: context.repo.owner, repo: context.repo.repo, page, per_page: 100 })).data;
+      const pagedReleases = ((await octokit.rest.repos.listReleases({ owner: context.repo.owner, repo: context.repo.repo, page, per_page: 100 })).data);
+
+      count = pagedReleases.length;
 
       releases.push(...pagedReleases.map(release => ({ tag: release.tag_name, branch: release.target_commitish, creation: Date.parse(release.created_at) })));
 
       page++;
 
-    } while (releases.some(release => release.branch === branch) && (branch === 'main' || releases.some(release => release.branch && 'main')));
+    } while (count > 0);
 
-    const prerelease = branch !== 'main';
+    const prerelease = target !== 'main';
 
-    const previousVersion = releases.filter(release => release.branch === branch).sort((a, b) => b.creation - a.creation).reverse().map(release => release.tag).pop();
+    const previousVersion = releases.filter(release => release.branch === target).sort((a, b) => b.creation - a.creation).reverse().map(release => release.tag).pop();
 
     const lastMainVersion = !prerelease ? previousVersion : releases.filter(release => release.branch === 'main').sort((a, b) => b.creation - a.creation).reverse()
-    
+
       .map(release => release.tag).pop();
 
     //TODO: Calculate the next version
-    const version = `v0.9.${Math.trunc(Math.random() * 10000)}`;
-
-    core.setOutput('commit', commit);
+    const version = `v0.9.${Math.trunc(Math.random() * 1000)}`;
 
     core.setOutput('version', version);
 
     core.setOutput('previousVersion', previousVersion);
+
+    if (detached || hotfix) {
+
+      const head = reference !== '' ? reference : source;
+
+      const title = `automated ${hotfix ? 'hotfix' : stage} release version ${version}`;
+
+      let pull = (await octokit.pulls.create({ owner: context.repo.owner, repo: context.repo.repo, base: target, head, title })).data;
+
+      while (pull.mergeable == null) {
+
+        await wait(5000);
+
+        pull = (await octokit.pulls.get({ owner: context.repo.owner, repo: context.repo.repo, pull_number: pull.number })).data;
+      }
+
+      if (!pull.mergeable) {
+
+        await octokit.pulls.update({ owner: context.repo.owner, repo: context.repo.repo, pull_number: pull.number, state: 'closed', title: `[failed] ${title}`});
+
+        throw new Error(`The source '${hotfix ? reference : source}' is not mergeable into '${target}'.`);
+      }
+
+      if (hotfix) {
+
+        octokit.pulls.create({ owner: context.repo.owner, repo: context.repo.repo, base: 'release', head, title });
+
+        octokit.pulls.create({ owner: context.repo.owner, repo: context.repo.repo, base: 'develop', head, title });
+      }
+
+      const merge = (await octokit.pulls.merge({ owner: context.repo.owner, repo: context.repo.repo, pull_number: pull.number, merge_method: 'merge' })).data;
+
+      if (merge.merged) {
+
+        core.setOutput('reference', merge.sha);
+
+      } else {
+
+        await octokit.pulls.update({ owner: context.repo.owner, repo: context.repo.repo, pull_number: pull.number, state: 'closed', title: `[failed] ${title}`});
+
+        throw new Error(`Failed to merge the pull request #${pull.number} '[failed] ${title}'.`);
+      }
+    }
 
   } catch (error) {
 
